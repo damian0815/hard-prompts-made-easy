184bf6546b7da67b0f0e2cebcd7884f9cc1323be
Damian Stewart <d@damianstewart.com>
2024-02-10 19:53:27 +1300

    fix bug where <bos> and <eos> where not respected

--- optim_utils.py
+++ optim_utils.py
@@ -18,6 +18,10 @@ from sentence_transformers.util import (semantic_search,
 
 from tqdm.auto import tqdm
 
+max_len = 77
+bos_token_id = 49406
+eos_token_id = 49407
+
 
 def read_json(filename: str) -> Mapping[str, Any]:
     """Returns a Python dict representation of JSON object at input file."""
@@ -111,27 +115,43 @@ def get_target_features(model, preprocess, tokenizer_funct, device, target_image
     return all_target_features
 
 
-def initialize_prompt(tokenizer, token_embedding, args, device, initial_prompt=None):
+def initialize_prompt(tokenizer, token_embedding, args, device, 
+                      initial_prompt=None, 
+                      prompt_template="{}"):
 
-    max_len = 77
-    bos = 49406
     bos_text = "<start_of_text>"
-    eos = 49407
+
+    
+    template_parts = prompt_template.split("{}")
+    if len(template_parts) != 2:
+        raise ValueError(f"invalid promt_template '{prompt_template}' - must contain " + "{}")
+    prefix = template_parts[0].strip()
+    suffix = template_parts[1].strip()
+
+    prefix_ids = tokenizer.encode(prefix)
+    suffix_ids = tokenizer.encode(suffix)
+    #print(f"prompt template '{prompt_template}' -> prefix ids {prefix_ids}, suffix ids {suffix_ids}")
     
     if initial_prompt is not None:
         initial_ids = tokenizer.encode(initial_prompt)
-        if (len(initial_ids)-2) > max_len:
-            raise ValueError(f"initial prompt '{initial_prompt}' is too long - has {len(initial_ids)} tokens, max {max_len-2}")
         prompt_len = len(initial_ids)
-        prompt_ids = torch.tensor([initial_ids] * args.prompt_bs).to(device)
+        prompt_ids = torch.tensor([[bos_token_id] + prefix_ids + initial_ids + suffix_ids + [eos_token_id]] * args.prompt_bs).to(device)
     else:
         prompt_len = min(args.prompt_len, max_len-2)
-        prompt_ids = torch.randint(len(tokenizer.encoder), (args.prompt_bs, prompt_len)).to(device)
-
+        prompt_ids = torch.stack([torch.cat(
+            [torch.tensor([bos_token_id] + prefix_ids), 
+             torch.randint(len(tokenizer.encoder), (prompt_len,)), 
+             torch.tensor(suffix_ids + [eos_token_id])
+            ]) for i in range(args.prompt_bs)])
+        #prompt_ids = torch.randint(len(tokenizer.encoder), (args.prompt_bs, prompt_len)).to(device)
+    
+    #prompt_ids = prefix_ids + initial_ids + suffix_ids
     prompt_embeds = token_embedding(prompt_ids).detach()
+    #print(f"-> prompt ids {prompt_ids} -> embeds {prompt_embeds}")
     prompt_embeds.requires_grad = True
 
-    dummy_ids = [bos] + prompt_len * [-1] + [eos]
+    dummy_ids = [bos_token_id] + prefix_ids + (prompt_len * [-1]) + suffix_ids + [eos_token_id]
+    assert len(dummy_ids) <= max_len
     dummy_ids += [0] * (max_len - len(dummy_ids))
     dummy_ids = torch.tensor([dummy_ids] * args.prompt_bs).to(device)
 
@@ -177,6 +197,7 @@ def optimize_prompt_loop(model, tokenizer, token_embedding,
                          all_target_features, args, device,
                          all_target_loss_scales=None, 
                          initial_prompt=None,
+                         prompt_template='{}',
                         noise_amount=0):
     if all_target_loss_scales is None:
         all_target_loss_scales = torch.ones(all_target_features.shape[0])
@@ -191,7 +212,8 @@ def optimize_prompt_loop(model, tokenizer, token_embedding,
 
     # initialize prompt
     prompt_embeds, dummy_embeds, dummy_ids = initialize_prompt(tokenizer, token_embedding, args, device, 
-                                                               initial_prompt=initial_prompt
+                                                               initial_prompt=initial_prompt,
+                                                               prompt_template=prompt_template
                                                               )
     p_bs, p_len, p_dim = prompt_embeds.shape
 
@@ -200,6 +222,11 @@ def optimize_prompt_loop(model, tokenizer, token_embedding,
 
     best_count = 10
     best = [None] * best_count
+
+    def decode_embeds(embeds):
+        _, indices = nn_project(embeds, token_embedding, print_hits=False)
+        return indices, decode_ids(indices, tokenizer)
+
 
     pbar = tqdm(range(opt_iters))
     for step in pbar:
@@ -218,15 +245,31 @@ def optimize_prompt_loop(model, tokenizer, token_embedding,
             
             # forward projection
             projected_embeds, nn_indices = nn_project(prompt_embeds, token_embedding, print_hits=False)
+            # sanity check
+            assert nn_indices[:, 0] == bos_token_id
+            assert nn_indices[:, -1] == eos_token_id
+            
+            #print("forward projection got", nn_indices)
+            proj_len = projected_embeds.shape[1]
+            target_train_indices = dummy_ids == -1
+            source_train_indices = target_train_indices[:, :proj_len]
+            
             if noise_amount > 0:
-                projected_embeds += torch.randn_like(projected_embeds) * noise_amount
-            #print("current: ", decode_ids(nn_indices, tokenizer))
-    
+                projected_embeds[source_train_indices] += torch.randn_like(projected_embeds[source_train_indices]) * noise_amount
+
             # get cosine similarity score with all target features
             with torch.no_grad():
                 # padded_embeds = copy.deepcopy(dummy_embeds)
                 padded_embeds = dummy_embeds.detach().clone()
-                padded_embeds[dummy_ids == -1] = projected_embeds.reshape(-1, p_dim)
+                #print(dummy_ids.shape)
+                #print(projected_embeds.shape, padded_embeds.shape, projected_embeds.reshape(-1, p_dim).shape)
+                #print("overwrite", target_train_indices)
+                #print(padded_embeds[target_train_indices].shape)
+                #print("vindaloo")
+                #print(projected_embeds[source_train_indices].shape)
+                #print("huh")
+                padded_embeds[target_train_indices] = projected_embeds[source_train_indices].reshape(-1, p_dim)
+                #print("evaluating score for:", decode_embeds(padded_embeds))    
                 logits_per_image, _ = model.forward_text_embedding(padded_embeds, dummy_ids, universal_target_features)
                 logits_per_image_scaled = logits_per_image * universal_loss_scales.unsqueeze(0).t()
                 #print("logits_per_image:", logits_per_image, ", scaled:", logits_per_image_scaled)
@@ -245,8 +288,10 @@ def optimize_prompt_loop(model, tokenizer, token_embedding,
             # padding
             # padded_embeds = copy.deepcopy(dummy_embeds)
             padded_embeds = dummy_embeds.detach().clone()
-            padded_embeds[dummy_ids == -1] = tmp_embeds.reshape(-1, p_dim)
-            
+            padded_embeds[dummy_ids == -1] = tmp_embeds[source_train_indices].reshape(-1, p_dim)
+
+            #print("doing loss with current padded:", decode_embeds(padded_embeds))    
+
             logits_per_image, _ = model.forward_text_embedding(padded_embeds, dummy_ids, target_features)
             cosim_scores = logits_per_image
             #print("cosine similarity:", cosim_scores)
@@ -273,8 +318,9 @@ def optimize_prompt_loop(model, tokenizer, token_embedding,
     
             curr_lr = input_optimizer.param_groups[0]["lr"]
             #cosim_scores = cosim_scores.mean().item()
-    
-            decoded_text = decode_ids(nn_indices, tokenizer)[best_indx]
+
+            # decode text, without <bos> and <eos> markers
+            decoded_text = decode_ids(nn_indices[:, 1:-1], tokenizer)[best_indx]
             pbar.set_postfix({'loss': loss.item(), 'curr': decoded_text}, )
             if print_step is not None and (step % print_step == 0 or step == opt_iters-1):
                 per_step_message = f"step: {step}, lr: {curr_lr}"
@@ -312,6 +358,7 @@ def optimize_prompt(model, preprocess, args, device,
                     target_prompts=None, 
                     target_loss_scales=None,
                     initial_prompt=None,
+                    prompt_template='{}',
                     noise_amount=0,
                     ):
     token_embedding = model.token_embedding
@@ -333,6 +380,7 @@ def optimize_prompt(model, preprocess, args, device,
                                            args, device,
                                          all_target_loss_scales=target_loss_scales,
                                          initial_prompt=initial_prompt,
+                                          prompt_template=prompt_template,
                                          noise_amount=noise_amount)
 
     return learned_prompt
